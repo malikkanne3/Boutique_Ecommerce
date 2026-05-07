b6249d84cb7eb40c1278c6bfee321233e56b62d0
malikkanne3 <malikkanne3@gmail.com>
2026-05-07 19:48:38 +0000

    Add comment to CommandeConfirmee class

--- app/Mail/CommandeConfirmee.php
+++ app/Mail/CommandeConfirmee.php
@@ -6,7 +6,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-
+//commandeconfirmer
 class CommandeConfirmee extends Mailable
 {
     use Queueable, SerializesModels;
